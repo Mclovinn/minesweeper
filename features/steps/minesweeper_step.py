a27619965adf46b1d03654224fe6db957e7eeed1
Agustin Varea <avarea@ascentio.com.ar>
2017-01-25 13:03:28 -0300

    Add board render dynamicly using BE information

--- features/steps/minesweeper_step.py
+++ features/steps/minesweeper_step.py
@@ -1,16 +1,27 @@
 from minesweeper import *
 from behave import *
 from splinter import Browser
+
 browser = Browser('flask',app=app)
+
 
 @given(u'Initial page')
 def step_impl(context):
 	browser.visit('http://127.0.0.1:5000')
 
+
 @then(u'Initial page showed')
 def step_impl(context):
 	assert 'Minesweeper' in browser.html
 
+
 @then(u'The game board is showed')
 def step_impl(context):
     assert browser.find_by_id('game_board')
+
+
+@then(u'The 3x3 game board is showed')
+def step_impl(context):
+    for x in range(1, 4):
+        for y in range(1, 4):
+            assert browser.find_by_id('position_{}_{}'.format(x, y))
--- model/models.py
+++ model/models.py
@@ -4,6 +4,16 @@ class MineSweeper:
     def __init__(self):
         self.board = Board()
 
+        rows = []
+        for x in range(1,4):
+            buttons = []
+            for y in range(1,4):
+                buttons.append({'position': 'position_{}_{}'.format(x, y)})
+
+            rows.append({'buttons': buttons})
+
+        self.minestable = {'rows': rows}
+
     def validate_position(self, position):
         self.board.markPosition(position)
         return self.board.get_board()
