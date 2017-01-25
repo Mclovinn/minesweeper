abaca5e1e4ad6bfb037240e7d0ea6bf295426720
Agustin Varea <avarea@ascentio.com.ar>
2017-01-25 11:38:19 -0300

    Add game board

--- features/steps/minesweeper_step.py
+++ features/steps/minesweeper_step.py
@@ -3,10 +3,14 @@ from behave import *
 from splinter import Browser
 browser = Browser('flask',app=app)
 
-@given(u'Initial State')
+@given(u'Initial page')
 def step_impl(context):
 	browser.visit('http://127.0.0.1:5000')
 
 @then(u'Initial page showed')
 def step_impl(context):
 	assert 'Minesweeper' in browser.html
+
+@then(u'The game board is showed')
+def step_impl(context):
+    assert browser.find_by_id('game_board')
